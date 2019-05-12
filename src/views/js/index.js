d2c532ed36ad162c97faa8fb663a508b939d24c3
Kabir Shah <shah5758@mydusd.org>
2019-05-12 13:58:25 -0700

    remove moon

--- src/views/js/index.js
+++
@@ -1,6 +0,0 @@
-import Moon from "./lib/moon.min.js";
-
-Moon({
-	root: "#root",
-	view: document.getElementById("template").textContent
-});
--- src/views/js/projects.js
+++
@@ -1,8 +0,0 @@
-import Moon from "./lib/moon.min.js";
-
-Moon({
-	root: "#root",
-    view: document.getElementById("template").textContent,
-    projects: [{image: "https://www.clker.com/cliparts/3/m/v/Y/E/V/small-red-apple-hi.png", name: "test project", description: "a project for testing display"}, 
-    {image: "https://www.clker.com/cliparts/3/m/v/Y/E/V/small-red-apple-hi.png", name: "test project", description: "a project for testing display"}]
-});
