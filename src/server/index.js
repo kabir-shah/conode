c006a3a0840560c29f11ed6bc6f5949e6a5d8690
Kabir Shah <shah5758@mydusd.org>
2019-05-13 19:00:58 -0700

    project schema

--- src/server/index.js
+++ src/server/index.js
@@ -2,13 +2,37 @@ const express = require("express");
 const expressHandlebars = require("express-handlebars");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-
 const path = require("path");
 
 const app = express();
+const hbs = expressHandlebars.create({});
+const Schema = mongoose.Schema;
 const port = 3000;
 
-const hbs = expressHandlebars.create({});
+mongoose.connect("mongodb://localhost/Conode", {
+	useCreateIndex: true,
+	useNewUrlParser: true
+});
+
+const ProjectSchema = new Schema({
+	id: {
+		type: String,
+		index: true,
+		unique: true
+	},
+	date: {
+		type: Date,
+		default: Date.now
+	},
+	title: String,
+	author: String,
+	description: String,
+	image: String,
+	content: String,
+	likes: Number
+});
+
+const Project = mongoose.model("Project", ProjectSchema);
 
 app.engine("handlebars", hbs.engine);
 app.set("views", path.resolve("src/views"));
@@ -18,12 +42,12 @@ app.use("/css", express.static(path.resolve("src/views/css")));
 app.use("/js", express.static(path.resolve("src/views/js")));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb://localhost/Conode", {
-	useNewUrlParser: true
-});
-
 app.get("/", (req, res) => {
-	res.render("index", { projects: [{title: "get rekt"}] });
+	Project.find().sort({ date: -1 }).limit(10).sort({ likes: -1 }).then(projects => {
+		res.render("index", { projects: [{title: "get rekt"}] });
+	}).catch(err => {
+		res.send("There was an error fetching the projects.");
+	});
 });
 
 app.listen(port, () => console.log(`Conode is running on port ${port}.`));
