ebccb0f2aa696d1f8157f8383e6c3ec5b89e7cae
Kabir Shah <shah5758@mydusd.org>
2019-05-13 18:29:14 -0700

    actually import body parser

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const expressHandlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const path = require("path");
