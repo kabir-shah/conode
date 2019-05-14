600b6c281ded6846ac43036dcb867548e58c451e
Kabir Shah <shah5758@mydusd.org>
2019-05-13 18:28:22 -0700

    basic server hopefully i haven't tested it

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,28 @@
 const express = require("express");
+const expressHandlebars = require("express-handlebars");
+const mongoose = require("mongoose");
+
+const path = require("path");
 
 const app = express();
 const port = 3000;
+
+const hbs = expressHandlebars.create({});
+
+app.engine("handlebars", hbs.engine);
+app.set("views", path.resolve("src/views"));
+app.set("view engine", "handlebars");
+
+app.use("/css", express.static(path.resolve("src/views/css")));
+app.use("/js", express.static(path.resolve("src/views/js")));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://localhost/Conode", {
+	useNewUrlParser: true
+});
+
+app.get("/", (req, res) => {
+	res.render("index", { projects: [{title: "get rekt"}] });
+});
 
 app.listen(port, () => console.log(`Conode is running on port ${port}.`));
